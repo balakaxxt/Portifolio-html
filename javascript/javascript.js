22c48ed2168c0aac75ce6faedcce7cbc1c0633c1
Dem_Oliveira <balakaxxt@gmail.com>
2022-09-29 11:52:00 -0300

    confirma

---
+++ javascript/javascript.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-btn')
+
+let header = document.querySelector('.header')
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+
+}
+
+let themeToggler = document.querySelector('#theme-toggler')
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-cog');
+}
